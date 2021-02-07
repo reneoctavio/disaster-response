973a143c6c169e8ede4f8532c290bb035545ac25
Rene Octavio Queiroz Dias <reneoctavio@gmail.com>
2021-02-07 12:05:27 -0300

    feat: Create NLPTransformer

---
+++ models/nlp_transformer.py
@@ -0,0 +1,48 @@
+import re
+import spacy
+
+
+class NLPTransformer:
+    """This class will transform text into tokenized forms
+
+    The text will go through a pipeline in a spaCy model
+    where it will be tokenized, have their POS determined,
+    and lemmatized.
+
+    After lemmatization, it will be through more cleaning.
+    It will have stop words removed, we will lower case the text,
+    and remove all non-alphanumeric tokens.
+
+    Attributes:
+        nlp (spacy.Language): A spaCy language model
+
+    """
+
+    def __init__(self):
+        spacy.prefer_gpu()
+        self.nlp = spacy.load("en_core_web_sm")
+
+    def cleaning(self, doc: spacy.tokens.Doc):
+        """Clean a Doc processed by a spaCy model."""
+        text = " ".join([token.lemma_ for token in doc if not token.is_stop])
+        text = text.lower()
+        text = re.sub(r"[^\w\s]", " ", text)
+        text = " ".join(text.split())
+        return text
+
+    def transform(self, texts: list, **transform_params):
+        """The transform method of sklearn Transform classes.
+
+        It will take a list of texts, and clean them for further
+        vectorization.
+
+        Args:
+            texts (list): A list of texts
+        """
+        transformed_texts = []
+
+        for doc in self.nlp.pipe(texts, disable=["parser", "ner"]):
+            transformed_texts.append(self.cleaning(doc))
+
+    def fit(self, X, y=None, **fit_params):
+        return self
